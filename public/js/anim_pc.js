2a204fb835f596500dae2626c9af2e4e309f9c0b
batsWTR <baptiste.wentzler@wanadoo.fr>
2025-10-03 20:09:49 +0200

    ajout js anim_pc 2

--- public/js/anim_pc.js
+++ public/js/anim_pc.js
@@ -2,5 +2,11 @@ console.log('anim pc')
 
 
 let fenetre = document.getElementById('anim_pc');
+let str = "il etait une fois le petit poucet";
+let arr = string.splice('');
 
-fenetre.innerText = "blablablablablablablablablablablabla blablablablabl<br>ablablablablablablablablablablabla et ruc muche un truc ca va hello";
+arr.foreach((el)=>{
+    fenetre.innerText = fenetre.innerText + el;
+});
+
+//fenetre.innerText = "blablablablablablablablablablablabla blablablablabl<br>ablablablablablablablablablablabla et ruc muche un truc ca va hello";
