5ceadd7b89f4733f516677ae48e133afdb695738
batsWTR <baptiste.wentzler@wanadoo.fr>
2025-10-04 11:30:40 +0200

    modif js

--- public/js/anim_pc.js
+++ public/js/anim_pc.js
@@ -3,9 +3,9 @@ console.log('anim pc')
 
 let fenetre = document.getElementById('anim_pc');
 let str = "il etait une fois le petit poucet";
-let arr = string.splice('');
+let arr = str.split('');
 
-arr.foreach((el)=>{
+arr.forEach((el)=>{
     fenetre.innerText = fenetre.innerText + el;
 });
 
