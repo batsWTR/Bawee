8c0a3d8ef52ddbeb5555d08de7a2ed03ff0981f6
batsWTR <baptiste.wentzler@wanadoo.fr>
2025-10-09 19:38:46 +0200

    page contact + model messagecontact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\Message;
 use Illuminate\Http\Request;
+use App\Models\MessageContact;
 
 class ContactController extends Controller
 {
@@ -12,6 +14,17 @@ class ContactController extends Controller
     }
 
     public function store(Request $request){
-        return view('contact');
+        $validated = $request->validate([
+            'email' => 'required|max:255',
+            'message' => 'required|min:6|max:4096'
+        ]);
+
+        //$message = MessageContact::create([
+        //    'email' => $validated->email,
+        //    'message' => $validated->message,
+         //   'user_id' => session()->getId(),
+        //]);
+
+        return redirect('contact')->with('status', 'Votre message a bien été pris en compte');
     }
 }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,16 +3,30 @@
 
 
 @section('main')
+    @if($errors->any())
+        <div class="alert alert-danger w-50 m-auto">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    @if(session('status'))
+        <div class="alert alert-success w-50 m-auto">
+            {{ session('status') }}
+        </div>
+    @endif
     <h1 class="mt-4 mb-3 text-center">Me contacter</h1>
     <form class="w-50 m-auto" action="{{ route('contact.store') }}" method="post">
         @csrf
         <div>
             <label class="form-label" for="email">Email</label>
-            <input class="form-control" type="email" name="email" id="email" required>
+            <input class="form-control" type="email" name="email" id="email" value="{{ old('email') }}"  required>
         </div>
         <div>
-            <label class="form-label" for="text">Votre message</label>
-            <textarea class="form-control" name="text" id="text" rows="8" cols="2" required>
+            <label class="form-label" for="message">Votre message</label>
+            <textarea class="form-control" name="message" id="message" rows="8" cols="2"  required>
         </textarea>
         </div>
         <button class="btn btn-primary mt-3" type="submit">Envoyer</button>
